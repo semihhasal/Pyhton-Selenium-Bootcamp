fd786d3225064a49e1a0a1dbe21a4e058316a638
Semihhasal <semih.hasal@htomail.com>
2023-03-11 22:43:19 +0300

    '2.gun'

---
+++ day2.py
@@ -0,0 +1,12 @@
+faiz=1.59
+vade=36
+krediAdı="İhtiyac Kredisi"
+
+
+krediler=["ihtiyac kredisi","Taşıt kredisi","Ev kredisi"]
+
+for kredi in krediler:
+    print(kredi)
+
+for i in range(len(krediler)):
+    print(krediler[i])
