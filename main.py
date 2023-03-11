1e7e925413273e3b2c9c625ce309db086f897c9a
Semihhasal <semih.hasal@htomail.com>
2023-03-11 10:04:36 +0300

    'odev1'

---
+++ main.py
@@ -0,0 +1,40 @@
+1-"-----------------------------------------PythonVeriTipleri-------------------------------------------"
+# String : Metinsel ifadelerin belirtilmesi icin  'char' veri tiplerinin bir araya gelmesiyle meydana gelen veri tipidir. 
+
+# Integer : Sayısal verileri ifade etmek icin kullanılan veri tipidir. Ondalikli sayilarin ifade edilmesi durumunda sadece sayinin tam kismini alir.
+
+# Float : Reel sayilarin ifade edilmesi icin kullanılır.
+
+# List : Farkli veri tiplerinin sadece tek bir degiskende toplanmasidir.
+
+# Tuple: Farkli veri tiplerinin tek bir degiskende toplanmasina ek olarak bu degiskende herhangi bir degisiklik yapilamamaktadır.
+
+# Dictionary: Birbiri ile iliskisi olan varlıkları bir degiskende yani köseli parantez icinde ifade edilmesidir.
+
+# Boolean : Sadece 1 bit yer kaplayan True veya False olarak ifade edilen veri tipidir.
+
+#2 "Kodlamaio sitesinin anasayfasinda yer alan birbirinden farkli Kurslarin ayri ayri cerveve seklinde siralanmasi bir dizidir. "Bu kurslarin isimlerinin ifade edilmesi metinsel ifade kursun süresi ve zamaninin belirtilmesi ise numerik bir ifadedir"
+# string: Sitede kullanılan tüm metinler için string veri tipi kullanılmıştır.
+# örneğin; Kurslarım,Kurs Programı
+# integer: Sitede derslerde olan ilerlememizi göstermesi için int komutu kullanılmıştır.
+# boolean:  "Bitir ve devam et" butonuna tıklanan alan bool veri tipi kullanılmıştır.
+
+#3 print("Sitemize Hoş geldiniz. Yapmak istediğiniz işlem için yukarıda yer alan kutulara tıklayabilirisiniz")
+
+#if ogrenciTamamlamaYuzdesi == 100:
+#   print("Kurs Başarıyla Tamamlandı")
+
+#else:
+#   print("Kurs Bitmedi")
+
+#if butunKurs == "(Kurs tamamlandı)
+#    print("Birinci Odev Tamamlandı")
+
+#else: 
+#   print("Birinci Odev tamamlanmadı Lutfen Odevı Yapınız")
+
+#if sifre =="123"
+#  print("Giris Basarılı")
+
+#else:
+#   print("Yanlis Sifre Girdiniz")
